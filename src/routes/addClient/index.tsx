57a106661b4353abf3121c6d48f12dd01a9d1e2c
Etarley <standecristo@gmail.com>
2023-04-17 11:34:59 -0400

    🔧 chore(package.json): update dependencies - Update "@types/react" from "18.0.33" to "18.0.35" - Update "@typescript-eslint/eslint-plugin" from "5.57.1" to "5.58.0" - Update "@typescript-eslint/parser" from "5.57.1" to "5.58.0" - Update "eslint" from "8.37.0" to "8.38.0" - Update "postcss" from "^8.4.21" to "^8.4.22" - Update "typescript" from "5.0.3" to "5.0.4" - Update "undici" from "5.21.0" to "5.21.2" - Update "vite-tsconfig-paths" from "4.0.8" to "4.2.0"
    🎨 style

--- src/routes/addClient/index.tsx
+++ src/routes/addClient/index.tsx
@@ -29,6 +29,7 @@ export default component$(() => {
                 type='text'
                 id='nombre'
                 name='nombre'
+                placeholder='John Doe'
                 class='w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-400'
               />
             </div>
@@ -37,6 +38,7 @@ export default component$(() => {
               <div class='flex'>
                 <input
                   type='number'
+                  placeholder='0.00'
                   id='balance'
                   name='balance'
                   class='w-full px-3 py-2 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-teal-400'
@@ -58,6 +60,7 @@ export default component$(() => {
               <label class='text-gray-700'>Próximo Pago:</label>
               <input
                 type='date'
+                placeholder='2021-01-01'
                 id='proximo-pago'
                 name='proximo-pago'
                 class='w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-400'
@@ -68,6 +71,7 @@ export default component$(() => {
               <div class='flex'>
                 <input
                   type='number'
+                  placeholder='0.00'
                   id='credito-disponible'
                   name='credito-disponible'
                   class='w-full px-3 py-2 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-teal-400'
@@ -95,6 +99,7 @@ export default component$(() => {
                   class='w-full px-3 py-2 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-teal-400'
                   step='0.01'
                   min='0'
+                  placeholder='0.00'
                 />
                 <select
                   name='interes-currency'
